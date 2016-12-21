7ee2ed9729ba06dd78ca8ff2117233abc134a8f3
Alexander Kudryavtsev <clear.meltdown@gmail.com>
2016-12-21 16:46:14 +0800

    Demos rework

--- src/anychart-angularjs.js
+++ src/anychart-angularjs.js
@@ -26,6 +26,51 @@ angular
         controller: ['$scope', '$element', AnychartController],
         link: AnychartLink
       };
+    }])
+    .directive('anygantt', [function() {
+      return {
+        restrict: 'EA',
+        replace: true,
+        scope: {
+          acType: '@',
+          acData: '=?',
+          acTitle: '@',
+          instance: '=?',
+          afterDraw: '=?',
+          acSplitterPosition: '@'
+        },
+        controller: ['$scope', '$element', AnyganttController],
+        link: AnychartLink
+      }
+    }])
+    .directive('anymap', [function() {
+      return {
+        restrict: 'EA',
+        replace: true,
+        scope: {
+          acType: '@',
+          acData: '=?',
+          acGeoData: '@',
+          acTitle: '@',
+          acLegend: '@',
+          instance: '=?',
+          afterDraw: '=?'
+        },
+        controller: ['$scope', '$element', AnymapController],
+        link: AnychartLink
+      }
+    }])
+    .directive('anystock', [function() {
+      return {
+        restrict: 'EA',
+        replace: true,
+        scope: {
+          instance: '=?',
+          afterDraw: '=?'
+        },
+        controller: ['$scope', '$element', AnystockController],
+        link: AnychartLink
+      }
     }]);
 
 
@@ -59,8 +104,9 @@ function AnychartController($scope, $element) {
 
     if ($scope.chart.data && $scope.acData)
       $scope.chart.data($scope.acData);
-    if ($scope.acTitle)
-      $scope.chart.title($scope.acTitle);
+
+    $scope.chart.title($scope.acTitle);
+
     if ($scope.acLegend)
       $scope.chart.legend($scope.acLegend !== 'false');
   }
@@ -74,4 +120,45 @@ function AnychartLink($scope, $element) {
     if ($scope.afterDraw)
       $scope.afterDraw.call($scope.chart, $scope.chart)
   });
+}
+
+
+function AnyganttController($scope, $element) {
+  this.$onInit = function() {
+    $scope.chart = $scope.instance || anychart[$scope.acType]();
+
+    if ($scope.chart.data && $scope.acData)
+      $scope.chart.data($scope.acData);
+
+    $scope.chart.title($scope.acTitle);
+
+    if ($scope.acSplitterPosition) {
+      if ($scope.acSplitterPosition.match(/^\d+%$/)) {
+        $scope.chart.splitterPosition($scope.acSplitterPosition)
+      } else if ($scope.acSplitterPosition.match(/^\d+$/)) {
+        $scope.chart.splitterPosition(+$scope.acSplitterPosition);
+      }
+    }
+  }
+}
+
+
+function AnymapController($scope, $element) {
+  this.$onInit = function() {
+    $scope.chart = $scope.instance || anychart[$scope.acType || 'map']($scope.acData);
+
+    $scope.chart.geoData($scope.acGeoData);
+    $scope.chart.title($scope.acTitle);
+
+    if ($scope.acLegend)
+      $scope.chart.legend($scope.acLegend !== 'false');
+
+  }
+}
+
+
+function AnystockController($scope, $element) {
+  this.$onInit = function() {
+    $scope.chart = $scope.instance;
+  }
 }
